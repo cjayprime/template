25359fffdf0e9981675ae74f2e69d632648447b8
Oreofe Olutola <oreofe.olutola@gmail.com>
2020-05-03 21:38:34 +0100

    style dateinput field

--- src/bundles/caseTriage/CreateTriage.js
+++ src/bundles/caseTriage/CreateTriage.js
@@ -1,12 +1,13 @@
 import React, { Fragment, useState } from 'react';
 
-import { Grid } from '@material-ui/core';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepButton from '@material-ui/core/StepButton';
-// import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
+import {
+  Grid,
+  Stepper,
+  Step,
+  StepButton,
+  Button,
+  Box
+} from '@material-ui/core';
 
 import Question from './components/question';
 
--- src/bundles/caseTriage/components/question.js
+++ src/bundles/caseTriage/components/question.js
@@ -1,43 +1,162 @@
 import React, { Fragment } from 'react';
 
-import { Grid } from '@material-ui/core';
+import { 
+  Grid, 
+  makeStyles, 
+  createMuiTheme, 
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  Typography,
+  OutlinedInput,
+  MenuItem
+} from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Typography from '@material-ui/core/Typography';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import { ThemeProvider } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    color: '#fff',
+    borderColor: 'white'
+  },
+  labelText: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    paddingTop: 10
+  },
+  container: {
+    marginBottom: 10
+  },
+  select: {
+    color: '#fff',
+    backgroundColor: '#474562',
+    '&:focus': {
+      borderColor: 'transparent !important'
+    },
+    '&$cssFocused': {
+      borderColor: 'transparent !important'
+    },
+    '&:hover': {
+      borderColor: 'transparent !important'
+    }
+  },
+  radio: {
+    color: '#FFFFFF'
+  },
+  selectElement: {
+    borderColor: 'transparent !important',
+    backgroundColor: '#474562'
+  },
+  cssOutlinedInput: {
+    '&$cssFocused $notchedOutline': {
+      borderColor: `${theme.palette.primary.main} !important`
+    },
+    backgroundColor: '#474562',
+    color: '#fff'
+  },
+  cssFocused: {},
+  notchedOutline: {
+    '&$cssFocused $notchedOutline': {
+      borderColor: `${theme.palette.primary.main} !important`
+    },
+    borderWidth: '1px',
+    borderColor: 'transparent !important',
+    color: 'white'
+  },
+  underline: {
+    backgroundColor: '#474562'
+  },
+  dateCssOutlinedInput: {
+    '&$cssFocused $notchedOutline': {
+      borderColor: `${theme.palette.primary.main} !important`
+    },
+    backgroundColor: '#474562',
+    color: '#fff',
+    // padding: '18.5px 14px;',
+    borderRadius: 10
+  }
+}));
+
+const dateInputTheme = createMuiTheme({
+  overrides: {
+    MuiFilledInput: {
+      root: {
+        backgroundColor: '#474562',
+        color: '#fff',
+        borderRadius: '8px !important',
+        border: '2px solid transparent',
+        '&:hover': {
+          backgroundColor: '#474562'
+        },
+        '&$focused': {
+          borderColor: '#fff',
+          backgroundColor: '#474562'
+        }
+      },
+      underline: {
+        backgroundColor: '#474562',
+        '&:before, &:after': {
+          display: 'none'
+        }
+      },
+      adornedEnd: {
+        paddingRight: 5
+      },
+      input: {
+        paddingTop: 18.5,
+        paddingBottom: 18.5,
+        borderRadius: 8,
+        height: 16.625,
+        lineHeight: 1
+      }
+    },
+    MuiFormControl: {
+      root: {
+        border: 0
+      },
+      marginNormal: {
+        margin: '0 !important'
+      }
+    }
+  }
+});
+
+const TransformIcon = () => <KeyboardArrowDown color="primary" />;
 
 const DateQuestionType = ({ question, questionKey, answer, onAnswer }) => {
   const selectedDate = answer ? new Date(answer) : null;
+  const classes = useStyles();
 
   return (
     <Fragment>
       <Grid container spacing={4}>
         <Grid item xs={4}>
-          <Typography>{question}</Typography>
+          <Typography className={classes.labelText}>{question}</Typography>
         </Grid>
         <Grid item md={8}>
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-              disableToolbar
-              variant="inline"
-              inputVariant="outlined"
-              format="dd/MM/yyyy"
-              margin="normal"
-              id={questionKey}
-              label="Date picker inline"
-              value={selectedDate}
-              onChange={date => onAnswer(questionKey, date)}
-              fullWidth={true}
-              KeyboardButtonProps={{
-                'aria-label': 'change date'
-              }}
-            />
+            <ThemeProvider theme={dateInputTheme}>
+              <KeyboardDatePicker
+                variant="inline"
+                inputVariant="filled"
+                format="dd/MM/yyyy"
+                margin="normal"
+                id={questionKey}
+                value={selectedDate}
+                onChange={date => onAnswer(questionKey, date)}
+                fullWidth={true}
+                views={['year', 'date', '']}
+                animateYearScrolling
+                autoOk
+                maxDate={new Date()}
+              />
+            </ThemeProvider>
           </MuiPickersUtilsProvider>
         </Grid>
       </Grid>
@@ -50,22 +169,45 @@ DateQuestionType.defaultProps = {
   onAnswer: () => {}
 };
 
+const InputTextComp = (classes, input) => {
+  const { type, answer, questionKey, onAnswer, placeholder } = input;
+  let rows = null;
+  const multiline = type === 'textArea' ? true : false;
+  if (multiline) rows = 5;
+
+  return (
+    <OutlinedInput
+      fullWidth
+      placeholder={placeholder || ''}
+      style={{ color: 'white' }}
+      multiline={multiline}
+      rows={rows}
+      value={answer}
+      onChange={e => onAnswer(questionKey, e.target.value)}
+      classes={{
+        root: classes.cssOutlinedInput,
+        focused: classes.cssFocused,
+        notchedOutline: classes.notchedOutline
+      }}
+    />
+  );
+};
+
 const StringQuestionType = ({ question, answer, questionKey, onAnswer }) => {
+  const classes = useStyles();
   return (
     <Fragment>
       <Grid container spacing={4}>
         <Grid item xs={4}>
-          <Typography>{question}</Typography>
+          <Typography className={classes.labelText}>{question}</Typography>
         </Grid>
         <Grid item md={8}>
-          <TextField
-            id="outlined-basic"
-            label="Outlined"
-            variant="outlined"
-            fullWidth={true}
-            value={answer}
-            onChange={e => onAnswer(questionKey, e.target.value)}
-          />
+          {InputTextComp(classes, {
+            type: 'text',
+            answer,
+            questionKey,
+            onAnswer
+          })}
         </Grid>
       </Grid>
     </Fragment>
@@ -77,15 +219,44 @@ StringQuestionType.defaultProps = {
   onAnswer: () => {}
 };
 
-const SelectQuestionType = ({ question }) => {
+const SelectQuestionType = ({ question, answer, questionKey, onAnswer }) => {
+  const classes = useStyles();
+  const values = [
+    {
+      label: 'label',
+      value: 'value'
+    }
+  ];
+
   return (
     <Fragment>
       <Grid container spacing={4}>
         <Grid item xs={4}>
-          <Typography>{question}</Typography>
+          <Typography className={classes.labelText}>{question}</Typography>
         </Grid>
         <Grid item md={8}>
-          select answer input
+          <TextField
+            select
+            fullWidth
+            value={answer}
+            onChange={e => onAnswer(questionKey, e.target.value)}
+            InputProps={{
+              className: classes.select
+            }}
+            SelectProps={{
+              native: false,
+              IconComponent: TransformIcon,
+              icon: classes.icon
+            }}
+            variant="outlined">
+            {values.map(option => (
+              <MenuItem
+                key={`${option.value}--${questionKey}`}
+                value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
         </Grid>
       </Grid>
     </Fragment>
@@ -104,15 +275,17 @@ const MultiChoiceQuestionType = ({
   answer,
   onAnswer
 }) => {
+  const classes = useStyles();
+
   return (
     <Fragment>
       <Grid container spacing={4}>
         <Grid item xs={4}>
-          <Typography>{question}</Typography>
+          <Typography className={classes.labelText}>{question}</Typography>
         </Grid>
         <Grid item md={8}>
           <RadioGroup
-            aria-label={questionKey}
+            style={{ display: 'flex', flexDirection: 'row' }}
             name={questionKey}
             value={answer}
             onChange={e => onAnswer(questionKey, e.target.value)}>
@@ -121,8 +294,18 @@ const MultiChoiceQuestionType = ({
                 <FormControlLabel
                   key={`${questionKey}-${option}`}
                   value={option}
-                  control={<Radio />}
+                  control={
+                    <Radio
+                      color="primary"
+                      classes={{
+                        colorPrimary: classes.radio
+                      }}
+                    />
+                  }
                   label={option}
+                  classes={{
+                    root: classes.radio
+                  }}
                 />
               ))}
             </Grid>
